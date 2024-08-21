289ac76fb25ea4762260015611f2819580b6836a
armaan535kh <kh.armaan535@gmail.com>
2024-08-20 22:00:43 -0700

    Initial Commit

---
+++ TabView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabView
+//
+//  Created by Armaan Khan  on 20/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Armaan Khan  on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
